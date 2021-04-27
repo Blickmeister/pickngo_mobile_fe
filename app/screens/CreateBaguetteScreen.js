cc3986ac060c52efa364d354cd759b9a9cd23cd2
Blickmeister <ondrablick@seznam.cz>
2021-04-28 00:17:11 +0200

    project structure reworked - login se asi ete presune pred vsechny pages - zatim jen pred vytvorenim objednavky; login system (snad uz) completed; createBaguette stuff added
    TODO createBugette stuff; rest of order (completeOrderPage etc.); a zbytek funkci..

--- app/screens/CreateBaguetteScreen.js
+++ app/screens/CreateBaguetteScreen.js
@@ -1,67 +1,133 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import RadioButtonRN from 'radio-buttons-react-native';
-import {getIngredientsUrl} from '../constants/endpoints';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {ASYNC_STORAGE_USER_KEY} from '../screens/LoginScreen';
+import CreateBaguetteComponent from '../components/createbaguette/CreateBaguetteComponent';
+import {createBaguetteOrderUrl} from '../constants/endpoints';
+import {Button} from 'react-native-paper';
 
 class CreateBaguetteScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            order: {
-                customer: {},
-                price: '',
-                date: '',
-                state: '',
-                items: [],
-            },
-            ingredients: [],
+            user: '',
+            orderId: '',
+            keyForRemount: 1,
         };
+        //const {cookies} = props;
+        //this.state.csrfToken = cookies.get('XSRF-TOKEN');
+        //this.login = this.login.bind(this);
+        //this.logout = this.logout.bind(this);
     }
 
+    getItem = () => {
+        return AsyncStorage.getItem(ASYNC_STORAGE_USER_KEY);
+    };
+
     componentDidMount() {
-        fetch(getIngredientsUrl, {
-            method: 'GET',
+        /* (() => {
+             let user = AsyncStorage.getItem(ASYNC_STORAGE_USER_KEY);
+             if (user !== null && user !== undefined && user !== '') {
+                 // We have data!!
+                 this.setState({user: user, isAuthenticated: true});
+                 console.log("USEROS:" + user);
+             } else {
+                 this.setState({isAuthenticated: false});
+             }
+         })
+         ();*/
+        (async () => {
+            await this.retrieveData();
+        })();
+        //console.log('orderId: ' + this.props.orderId);
+        //if (this.props.orderId === undefined) {
+        // nemáme ještě orderId -> nová objednávka
+        fetch(createBaguetteOrderUrl, {
+            credentials: 'include',
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Credentials': true,
-                'Access-Control-Allow-Origin': 'http://192.168.100.12:3000',
+                'Access-Control-Allow-Origin': '*',
             },
         })
-            .then((response) => response.json())
-            .then((jsonResponse) => {
-                this.setState({ingredients: jsonResponse});
-                console.log('response: ' + jsonResponse);
-            }).catch((err) => console.error(err));
+            .then((response) => {
+                if (response.ok) {
+                    console.log('Objednávka vytvořena');
+                    response.json()
+                        .then((jsonResponse) => {
+                            this.setState({orderId: jsonResponse.id});
+                            console.log('resík pusík: ' + jsonResponse.id);
+                        });
+                } else {
+                    response.json()
+                        .then((jsonResponse) =>
+                            console.error('Objednávku se nepodařilo vytvořit: ' + jsonResponse.message));
+                }
+            })
+            .catch((error) => console.log('Chyba při vytváření objednávky: ' + error));
+        //  } else {
+        // předání orderId pro vytvoření další bagety
+        //  this.setState({orderId: this.props.orderId});
+        //  }
     }
 
+    retrieveData = async () => {
+        try {
+            const value = await AsyncStorage.getItem(ASYNC_STORAGE_USER_KEY);
+            if (value !== null) {
+                console.log('VALUE: ' + value);
+                this.setState(() => ({user: value}));
+                // value previously stored
+            }
+        } catch (e) {
+            // error reading value
+            console.log('READ ERROR: ' + e);
+        }
+    };
+
+    logout() {
+        /* fetch('http://192.168.100.12:8080/api/logout', {
+             method: 'POST',
+             credentials: 'include',
+             headers: {
+                 'X-XSRF-TOKEN': this.state.csrfToken
+             }
+         })
+             .then(res => res.json())
+             .then(jsonResponse => {
+                 window.location.href = jsonResponse.logoutUrl + "?id_token_hint=" +
+                     jsonResponse.idToken + "&post_logout_redirect_uri=" + window.location.origin;
+             }).catch((err) => console.error(err));*/
+    }
+
+    addNextBaguetteHandler = () => {
+        this.setState(({keyForRemount}) => ({
+            keyForRemount: keyForRemount + 1,
+        }));
+    };
+
+    baguetteOrderCancelHandler = () => {
+        // smazání baguetteOrder
+
+        // redirect to Home
+    };
+
     render() {
-        const baguetteTypes = [{
-            label: 'Světlá',
-        }, {
-            label: 'tmavá',
-        },
-        ];
+        let definedUser = this.state.user !== undefined;
+
         return (
             <View style={styles.container}>
                 <View style={styles.containerWelcome}>
-                    <Text style={styles.welcome}>PickNGo</Text>
+                    <Text style={styles.welcome}>PickNGo - Návrh bagety</Text>
                 </View>
-                <View style={styles.typeContainer}>
-                    <Text>Typ bagety:</Text>
-                    {/* zatím na pevno, pak dynamicky */}
-                    <RadioButtonRN
-                        data={baguetteTypes}
-                        selectedBtn={(e) => console.log(e)}
-                    />
-                </View>
-                <View style={styles.ingredientContainer}>
-                    <Text>Ingredience:</Text>
-                    {/* TODO */}
-                    {this.state.ingredients.map((ingredient, index) => {
-                        return (
-                            <Text>{ingredient.name}</Text>
-                        )
-                    })}
+                {definedUser && <Text style={styles.textCenter}>Přihlášený uživatel: {this.state.user}</Text>}
+                <CreateBaguetteComponent key={this.state.keyForRemount} orderId={this.state.orderId}/>
+                <View style={styles.containerRow}>
+                    <Button mode="contained" color="blue" onPress={this.addNextBaguetteHandler}>Přidat další
+                        bagetu</Button>
+                    <Button mode="contained" color="green">Souhrn objednávky</Button>
+                    <Button mode="contained" color="red" onPress={this.baguetteOrderCancelHandler}>Zrušit</Button>
                 </View>
             </View>
         );
@@ -73,8 +139,8 @@ export default CreateBaguetteScreen;
 // stylizace
 const styles = StyleSheet.create({
     container: {
-        marginTop: 10,
-        marginLeft: 10,
+        padding: 10,
+        flex: 1,
     },
     containerWelcome: {
         justifyContent: 'center',
@@ -99,5 +165,18 @@ const styles = StyleSheet.create({
     },
     ingredientContainer: {
         marginTop: 10,
+    },
+    textCenter: {
+        textAlign: 'center',
+    },
+    textBig: {
+        fontSize: 14,
+        fontWeight: 'bold',
+    },
+    marginAll: {
+        margin: 2,
+    },
+    containerRow: {
+        flexDirection: 'row',
     },
 });
